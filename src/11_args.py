1aa90d08e7e4b60e49010650a2ab662746b4906a
WilliamBerlin76 <william.berlin76@gmail.com>
2020-02-10 22:19:13 -0600

    completed args

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(n1, n2):
+    return n1 + n2
 
 print(f1(1, 2))
 
@@ -13,6 +15,8 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*args):
+    return sum(args)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -22,6 +26,10 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
+
+def f2(arr):
+    return sum(arr)
+
 print(f2(a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
@@ -30,6 +38,11 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(a, b=None):
+    if b == None:
+        return a + 1
+    else:
+        return a + b
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,7 +57,9 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
-
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print('key: %s, value: %s' %(key, value))
 # Should print
 # key: a, value: 12
 # key: b, value: 30
@@ -62,4 +77,8 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
+def f4(dtn):
+    for key, value in dtn.items():
+        print('key: %s, value: %s' %(key, value))
+
 f4(d)
